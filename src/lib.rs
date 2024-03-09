5cd9824dfa889dda3fd8003fbee3ea7081772c15
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-03-09 16:57:39 +1000

    todo: display touch events

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,36 @@
 use bevy::{app::PluginGroupBuilder, prelude::*};
+use bevy_editor_pls::prelude::*;
 
 pub struct InfiMapPlugins;
 
 impl PluginGroup for InfiMapPlugins {
     fn build(self) -> PluginGroupBuilder {
-        PluginGroupBuilder::start::<Self>().add(TestingPlugin)
+        PluginGroupBuilder::start::<Self>()
+            .add(TestingPlugin)
+            .add(EditorPlugin::default())
     }
 }
 
 pub struct TestingPlugin;
 
+#[derive(Resource, Reflect, Default, Deref, DerefMut)]
+pub struct CustomTouches(Vec<TouchInput>);
+
 impl Plugin for TestingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, touch_system);
+        app.add_systems(Update, (touch_system, touch_event_system))
+            .register_type::<CustomTouches>()
+            .init_resource::<CustomTouches>();
+    }
+}
+
+fn touch_event_system(
+    mut touch_events: EventReader<TouchInput>,
+    mut touches: ResMut<CustomTouches>,
+) {
+    for event in touch_events.read() {
+        info!("Touch Event: {:?}", event);
+        touches.push(*event);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,18 @@ use infi_map::InfiMapPlugins;
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins((DefaultPlugins, InfiMapPlugins));
+    app.add_plugins((
+        DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+							title: "InfiMap Application".into(),
+							canvas: Some("#app".into()),
+							prevent_default_event_handling: false,
+							..default()
+						}),
+            ..default()
+        }),
+        InfiMapPlugins,
+    ));
 
     app.run();
 }
