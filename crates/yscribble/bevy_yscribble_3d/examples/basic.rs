2bc55249106920cdbf15a030ab52b435ff739cc9
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-06-14 16:15:58 +1000

    updated blender

--- crates/yscribble/bevy_yscribble_3d/examples/basic.rs
+++ crates/yscribble/bevy_yscribble_3d/examples/basic.rs
@@ -25,7 +25,7 @@ fn main() {
 			YScribble3DPlugins,
 			bevy_editor_pls::EditorPlugin::default(),
 		))
-		.insert_resource(DebugPickingMode::Normal)
+		.insert_resource(DebugPickingMode::Disabled)
 		.add_systems(Startup, setup)
 		.run();
 }
@@ -34,19 +34,19 @@ fn setup(
 	mut commands: Commands,
 	mut meshs: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
-	mut ass: ResMut<AssetServer>,
+	ass: Res<AssetServer>,
 ) {
 	commands.spawn(Camera3dBundle {
-		transform: Transform::from_translation(Vec3::new(0.0, 10.0, 1.0))
+		transform: Transform::from_translation(Vec3::new(0.0, 30.0, 1.0))
 			.looking_at(Vec3::ZERO, Vec3::Y),
 		..default()
 	});
 
-	let width = 10.0;
+	let radius = 10.0;
 	commands
 		.spawn((
 			PbrBundle {
-				mesh: meshs.add(Cuboid::new(width * 2.0, 1.0, width * 2.0)),
+				mesh: meshs.add(Cuboid::new(radius * 2.0, 1.0, radius * 2.0)),
 				material: materials.add(Color::GRAY),
 				..default()
 			},
@@ -57,18 +57,23 @@ fn setup(
 		.with_children(|parent| {
 			parent.spawn((
 				PbrBundle {
-					mesh: meshs.add(Cuboid::new(10.0, 0.1, 0.2)),
+					mesh: meshs.add(Cuboid::new(radius * 2.0, 0.1, 0.2)),
 					material: materials.add(Color::WHITE),
-					transform: Transform::from_translation(Vec3::new(0.0, 1.1, -10.1)),
+					transform: Transform::from_translation(Vec3::new(0.0, 1.1, -radius * 2.1)),
 					..default()
 				},
 				Name::new("Pad Outline Top"),
 			));
 
-			parent.spawn(SceneBundle {
-				scene: ass.load("blender/Arrow.glb"),
-				..default()
-			});
+			parent.spawn((
+				SceneBundle {
+					// arrow points in -z direction
+					transform: Transform::IDENTITY,
+					scene: ass.load("blender/Arrow.glb#Scene0"),
+					..default()
+				},
+				Name::new("Arrow Model"),
+			));
 		});
 }
 
@@ -78,22 +83,49 @@ fn on_drag_start(
 ) {
 	let pad_entity = event.listener();
 	let event_data: &Pointer<DragStart> = event.deref();
-	debug_assert_eq!(pad_entity, event_data.target);
+	// debug_assert_eq!(pad_entity, event_data.target); // blocking entities trigger this assert
 
-	// the magic line
-	// assumes that the hit is on the primary surface of the entity
-	// todo: use [GlobalTransform] and the normal from the event to remove events on the edges
-	let pos = event_data.pointer_location.position;
-	// todo: map onto surface properly in different orientations
-	let pos = ScribblePos {
-		center_x: pos.x,
-		center_y: pos.y,
-	};
+	let world_point = event_data.event.hit.position;
+	let world_normal = event_data.event.hit.normal;
 
-	match event_data.pointer_id {
-		PointerId::Mouse => {
-			emitted_events.send(InputEventRaw::MouseStart { pad_entity, pos });
+	if let Some(world_point) = world_point {
+		if let Some(world_normal) = world_normal {
+			// check normals here to make sure people don't click the underside / edges
+			let expected = Vec3::Y;
+			if world_normal.dot(expected) != 1.0 {
+				// todo: ignore the arrow from the event input
+				warn!(message = "A DragStart event was received, but it appears to not be the expected normal", note = "This is likely because the user didn't click the primary face", ?expected, ?world_normal)
+			}
+		} else {
+			debug!(message = "No normals received from DragStart event", ?event)
 		}
-		_ => todo!(),
+
+		// todo: actually compute stuff
+		let pos = ScribblePos {
+			center_x: world_point.x,
+			center_y: world_point.z,
+		};
+
+		match event_data.pointer_id {
+			PointerId::Mouse => {
+				emitted_events.send(InputEventRaw::MouseStart { pad_entity, pos });
+			}
+			_ => todo!(),
+		}
+	} else {
+		warn!(
+			message = "Received DragStart event with no position?",
+			?event
+		);
 	}
+
+	// // the magic line
+	// // assumes that the hit is on the primary surface of the entity
+	// // todo: use [GlobalTransform] and the normal from the event to remove events on the edges
+	// let pos = event_data.pointer_location.position;
+	// // todo: map onto surface properly in different orientations
+	// let pos = ScribblePos {
+	// 	center_x: pos.x,
+	// 	center_y: pos.y,
+	// };
 }
