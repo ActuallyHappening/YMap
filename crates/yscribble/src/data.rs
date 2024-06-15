8d878f996b6ccac69c21fdf05d2b5c0d739f4e57
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-06-15 16:33:43 +1000

    refactor: partial_line is now an `Option`

--- crates/yscribble/src/data.rs
+++ crates/yscribble/src/data.rs
@@ -9,9 +9,7 @@ pub struct ScribbleData {
 	complete_lines: Vec<CompleteLine>,
 
 	/// Ideally should be processed into complete lines soon.
-	/// 
-	/// The first element, `.first()`, is the currently active one.
-	pub partial_lines: Vec<PartialLine>,
+	pub partial_line: Option<PartialLine>,
 }
 
 impl ScribbleData {
@@ -19,26 +17,41 @@ impl ScribbleData {
 	pub fn new(data: impl Iterator<Item = CompleteLine>) -> Self {
 		ScribbleData {
 			complete_lines: data.collect(),
-			partial_lines: Default::default(),
+			partial_line: Default::default(),
 		}
 	}
 
 	pub fn push_partial_point(&mut self, point: ScribblePoint) {
-		if let Some(last) = self.partial_lines.first_mut() {
+		if let Some(last) = &mut self.partial_line {
 			last.push(point);
 		} else {
 			let mut new_line = PartialLine::new();
 			trace!(message = "Creating new partial line for point", ?point);
 			new_line.push(point);
-			self.partial_lines.push(new_line);
+			self.partial_line = Some(new_line);
 		}
 	}
 
 	/// Call to begin a new partial line.
 	/// Useful for building up a cache of [PartialLine]s over time to later process.
+	/// 
+	/// Discards excess [ScribblePoint]s using [PartialLine::try_consolidate].
 	pub fn cut_line(&mut self) {
 		trace!(message = "Cutting into a new line");
-		self.partial_lines.insert(0, PartialLine::new());
+		let Some(partial_line) = self.partial_line.take() else {
+			return;
+		};
+		match partial_line.try_consolidate() {
+			Ok(complete_line) => {
+				trace!(message = "Consolidated partial line", ?complete_line);
+				self.complete_lines.push(complete_line);
+			}
+			Err(partial_line) => {
+				debug!(message = "Partial line not yet complete", ?partial_line);
+				// discards partial line
+				// self.partial_line = Some(partial_line);
+			}
+		}
 	}
 
 	pub fn extend_completed(&mut self, data: impl Iterator<Item = CompleteLine>) {
@@ -47,20 +60,5 @@ impl ScribbleData {
 
 	pub fn iter_complete(&self) -> impl Iterator<Item = &CompleteLine> {
 		self.complete_lines.iter()
-	}
-
-	/// Replaces with an empty [Vec]
-	fn take_partial_lines(&mut self) -> Vec<PartialLine> {
-		std::mem::take(&mut self.partial_lines)
-	}
-
-	/// Attempts to convert [ScribbleData::partial_lines] into [ScribbleData::complete_lines].
-	/// Throws away any [PartialLine]s that cannot be converted.
-	pub fn consolidate(&mut self) {
-		let consolidated = self
-			.take_partial_lines()
-			.into_iter()
-			.filter_map(|line| PartialLine::try_consolidate(line).ok());
-		self.extend_completed(consolidated);
 	}
 }
