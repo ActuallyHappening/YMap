01074ea43706b2b5f540b582d41b2bb0e1bd77c1
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-06-22 14:45:01 +1000

    add: lib.rs

---
+++ crates/xcode_build/src/lib.rs
@@ -0,0 +1,65 @@
+use std::env;
+
+use color_eyre::{
+	eyre::{eyre, Context as _, Report},
+	Section as _,
+};
+
+pub enum Archs {
+	x86_64,
+	arm64,
+}
+
+pub fn parse_archs() -> color_eyre::Result<Archs> {
+	impl Archs {
+		fn try_from_str(str: &str) -> Result<Self, Report> {
+			Ok(match str {
+				"x86_64" => Archs::x86_64,
+				"arm64" => Archs::arm64,
+				_ => {
+					return Err(eyre!("Cannot parse ARCHS env variable").note(format!("ARCHS: {:?}", str)))
+				}
+			})
+		}
+	}
+
+	match env::var("ARCHS") {
+		Err(e) => Err(e).wrap_err("No ARCHS env var present")?,
+		Ok(archs) => Archs::try_from_str(&archs),
+	}
+}
+
+pub fn rustc(target: &'static str, release: bool) -> Result<(), Report> {
+	let mut rustc = bossy::Command::pure("cargo").with_args([
+		"rustc",
+		"--crate-type",
+		"staticlib",
+		"--lib",
+		"--target",
+		target,
+	]);
+	if release {
+		rustc.add_arg("--release");
+	}
+	rustc
+		.run_and_wait()
+		.wrap_err("rustc invocation failed, likely a Rust-side build error")?;
+	Ok(())
+}
+
+pub fn install_tracing() {
+	use tracing_error::ErrorLayer;
+	use tracing_subscriber::prelude::*;
+	use tracing_subscriber::{fmt, EnvFilter};
+
+	let fmt_layer = fmt::layer().with_target(false);
+	let filter_layer = EnvFilter::try_from_default_env()
+		.or_else(|_| EnvFilter::try_new("info"))
+		.unwrap();
+
+	tracing_subscriber::registry()
+		.with(filter_layer)
+		.with(fmt_layer)
+		.with(ErrorLayer::default())
+		.init();
+}
--- crates/xcode_build/src/main.rs
+++ crates/xcode_build/src/main.rs
@@ -3,13 +3,11 @@ use color_eyre::{
 	eyre::{eyre, Context, Report},
 	Section, SectionExt,
 };
-use std::{
-	env,
-	ffi::OsStr,
-	path::PathBuf,
-};
+use std::{env, ffi::OsStr, path::PathBuf};
 #[allow(unused_imports)]
 use tracing::{debug, error, info, trace, warn};
+
+use xcode_build::*;
 
 fn main() -> Result<(), color_eyre::Report> {
 	// init error handling and tracing
@@ -19,14 +17,14 @@ fn main() -> Result<(), color_eyre::Report> {
 	}
 
 	let mut release_profile = false;
-	if env::var_os("CONFIGURATION") == Some(String::from("Debug").into()) {
+	if env::var("CONFIGURATION") == Ok(String::from("Debug").into()) {
 		release_profile = true;
 	}
 
 	// Add `$HOME/.cargo/bin` to PATH env variable
 	// Adds /opt/homebrew/bin to PATH env variable
 	{
-		if let Some(path) = env::var_os("PATH") {
+		if let Ok(path) = env::var("PATH") {
 			let mut paths = env::split_paths(&path).collect::<Vec<_>>();
 
 			let home_dir = &dirs::home_dir().unwrap_or("~".into());
@@ -54,14 +52,13 @@ fn main() -> Result<(), color_eyre::Report> {
 		// In this case, we need to add an extra library search path for build scripts and proc-macros,
 		// which run on the host instead of the target.
 		// (macOS Big Sur does not have linkable libraries in /usr/lib/.)
-		if let Some(developer_sdk_dir) = env::var_os("DEVELOPER_SDK_DIR") {
-			let developer_sdk_dir = Utf8PathBuf::try_from(PathBuf::from(developer_sdk_dir))
-				.wrap_err("DEVELOPER_SDK_DIR not UTF8")?;
+		if let Ok(developer_sdk_dir) = env::var("DEVELOPER_SDK_DIR") {
+			let developer_sdk_dir = Utf8PathBuf::from(developer_sdk_dir);
 			// check if directory exits
 			let exists = developer_sdk_dir.try_exists();
 			match exists {
 				Ok(true) => {
-					let library_path = env::var_os("LIBRARY_PATH").unwrap_or_default();
+					let library_path = env::var("LIBRARY_PATH").unwrap_or_default();
 					let mut library_paths = env::split_paths(&library_path).collect::<Vec<_>>();
 					library_paths.push(PathBuf::from(format!(
 						"{}/MacOSZ.sdk/usr/lib",
@@ -77,10 +74,16 @@ fn main() -> Result<(), color_eyre::Report> {
 				_ => {
 					return Err(
 						eyre!("DEVELOPER_SDK_DIR does not exist or not UTF8")
+        .note("The env variable DEVELOPER_SDK_DIR was found, but the path it pointed to doesn't exist")
 							.with_section(|| developer_sdk_dir.header("DEVELOPER_SDK_DIR:")),
 					)
 				}
 			}
+		} else {
+			info!(
+				message = "No DEVELOPER_SDK_DIR env variable found in environment",
+				note = "Not updating LIBRARY_PATH env variable"
+			);
 		}
 	}
 
@@ -96,7 +99,7 @@ fn main() -> Result<(), color_eyre::Report> {
 	}
 
 	match parse_archs()? {
-		Archs::arm64 => {
+		Archs::x86_64 => {
 			if is_simulator {
 				return Err(eyre!(
 					"Building for x86_64 but not on a simulator. This is not yet supported"
@@ -104,70 +107,21 @@ fn main() -> Result<(), color_eyre::Report> {
 			}
 
 			// Intel iOS simulator
+			// tbh I don't know what this does yet, haven't bothered removing it
 			env::set_var("CFLAGS_x86_64_apple_ios", "-targetx86_64-apple-ios");
 
-			let mut rustc = bossy::Command::pure("cargo").with_args([
-				"rustc",
-				"--crate-type",
-				"staticlib",
-				"--lib",
-				"--target",
-				"x86_64-apple-ios",
-			]);
-			if release_profile {
-				rustc.add_arg("--release");
+			rustc("x86_64-apple-ios", release_profile)?;
+		}
+		Archs::arm64 => {
+			if is_simulator {
+				// M1 iOS simulator
+				rustc("aarch64-apple-ios-sim", release_profile)?;
+			} else {
+				// Hardware iOS
+				rustc("aarch64-apple-ios", release_profile)?;
 			}
-			rustc.run_and_wait() ;
 		}
 	}
 
 	Ok(())
-}
-
-enum Archs {
-	x86_64,
-	arm64,
-}
-
-fn parse_archs() -> color_eyre::Result<Archs> {
-	impl Archs {
-		fn try_from_str(str: &str) -> Result<Self, Report> {
-			Ok(match str {
-				"x86_64" => Archs::x86_64,
-				"arm64" => Archs::arm64,
-				_ => {
-					return Err(eyre!("Cannot parse ARCHS env variable").note(format!("ARCHS: {:?}", str)))
-				}
-			})
-		}
-
-		fn try_from_os_str(str: &OsStr) -> Result<Self, Report> {
-			match str.to_str() {
-				Some(str) => Self::try_from_str(str),
-				None => Err(eyre!("ARCHS env variable is not UTF8")),
-			}
-		}
-	}
-
-	match env::var_os("ARCHS") {
-		None => Err(eyre!("No ARCHS env var present")),
-		Some(archs) => Archs::try_from_os_str(&archs),
-	}
-}
-
-fn install_tracing() {
-	use tracing_error::ErrorLayer;
-	use tracing_subscriber::prelude::*;
-	use tracing_subscriber::{fmt, EnvFilter};
-
-	let fmt_layer = fmt::layer().with_target(false);
-	let filter_layer = EnvFilter::try_from_default_env()
-		.or_else(|_| EnvFilter::try_new("info"))
-		.unwrap();
-
-	tracing_subscriber::registry()
-		.with(filter_layer)
-		.with(fmt_layer)
-		.with(ErrorLayer::default())
-		.init();
 }
