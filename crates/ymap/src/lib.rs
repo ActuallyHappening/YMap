e250c531c3602c1190a89097f5997b15385efe74
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-06-29 11:55:26 +1000

    fix: logging is now cleaner for primary ymap projec6t

--- crates/ymap/src/lib.rs
+++ crates/ymap/src/lib.rs
@@ -1,4 +1,8 @@
-use bevy::{app::PluginGroupBuilder, log::LogPlugin, prelude::*};
+use bevy::{
+	app::PluginGroupBuilder,
+	log::{BoxedSubscriber, LogPlugin},
+	prelude::*,
+};
 use bevy_cosmic_edit::{CosmicEditPlugin, CosmicFontConfig};
 use tracing::Level;
 
@@ -42,8 +46,27 @@ impl PluginGroup for YMapPlugins {
 pub fn main() {
 	let mut app = App::new();
 
+	// disables colour in XCode terminal, see https://github.com/bevyengine/bevy/pull/13984
 	#[cfg(feature = "ios")]
 	std::env::set_var("NO_COLOR", "1");
+
+	/// Returns a tracing filter that suppresses winit's noisy logs.
+	/// Works with winit 0.29, will probably break in 0.30 and beyond because
+	/// of module refactors.
+	fn winit_noisy_filter() -> tracing_subscriber::filter::Targets {
+		tracing_subscriber::filter::Targets::new()
+			.with_target(
+				"winit::platform_impl::platform::app_state",
+				tracing_subscriber::filter::LevelFilter::ERROR,
+			)
+			.with_default(tracing_subscriber::filter::LevelFilter::TRACE)
+	}
+
+	/// Will need to be updated in bevy 0.14
+	fn update_subscriber(subscriber: BoxedSubscriber) -> BoxedSubscriber {
+		use tracing_subscriber::prelude::*;
+		Box::new(subscriber.with(winit_noisy_filter()))
+	}
 
 	#[cfg_attr(not(feature = "ios"), allow(unused_mut))]
 	let mut default_plugins = DefaultPlugins
@@ -77,7 +100,7 @@ pub fn main() {
 			// filter: "ymap=trace,cosmic_text=trace,bevy_cosmic_edit=trace".into(),
 			// filter: r#"ymap=trace,[{message="processing non `RedrawRequested` event after the main event loop: AboutToWait"}]=error,[{message="processing `RedrawRequested` during the main event loop"}]=error"#.into(),
 			filter: "ymap=trace,bevy_replicon=debug".into(),
-			..default()
+			update_subscriber: Some(update_subscriber),
 		});
 
 	#[cfg(feature = "ios")]
