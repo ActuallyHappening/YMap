96e85484e8b989497839ab15018d7d892218bf50
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-07-03 19:57:21 +1000

    feat: auth tests are comprehensive!

--- src/auth.rs
+++ src/auth.rs
@@ -191,7 +191,7 @@ mod test {
 		// let users: Vec<serde_json::Value> = db.select(auth_config.users_table()).await?;
 		// assert_eq!(users.len(), 0, "Users table should not be readable until you have signed in");
 
-		for i in 1..10 {
+		for i in 1..5 {
 			let mut credentials = SignUp::testing_rand();
 			credentials.email =
 				yauth::types::Email::from_str(&format!("testgenerated{i}@me.com")).unwrap();
@@ -199,7 +199,8 @@ mod test {
 			// sign into scope account to create new user, then sign into root user
 			auth_config.sign_up(&db, &credentials).await?;
 			auth_config.invalidate(&db).await?;
-			conn_config.root_sign_in(&db).await?;
+			// basically signing into root here, since the default is no authentication
+			// conn_config.root_sign_in(&db).await?;
 
 			let users: Vec<serde_json::Value> = db.select(auth_config.users_table()).await?;
 			assert_eq!(
