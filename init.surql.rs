d649c0a73fda571f760a6794d48a5cd533b5d4b9
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-07-04 16:53:18 +1000

    add: test for db init valid

--- init.surql.rs
+++ init.surql.rs
@@ -1,2 +1,11 @@
 #[allow(dead_code)]
 pub const INIT_SURQL: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/init.surql"));
+
+#[test]
+fn db_init_is_valid() {
+	let file_path = camino::Utf8PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/init.surql"));
+	bossy::Command::impure("surreal")
+		.with_args(["validate", file_path.as_str()])
+		.run_and_wait()
+		.unwrap();
+}
