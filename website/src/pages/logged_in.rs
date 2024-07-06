14cbcead43f87007de2d6f1437084c470ef30bc3
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-07-06 16:41:36 +1000

    todo: add gobal state for database

--- website/src/pages/logged_in.rs
+++ website/src/pages/logged_in.rs
@@ -2,6 +2,10 @@ use crate::prelude::*;
 
 #[component]
 pub fn LoggedIn() -> impl IntoView {
+    create_effect(move |_| {
+        info!("Loaded the Logged in page");
+        let session_info = 
+    });
 	view! {
 		<h1>"Logged In"</h1>
 	}
