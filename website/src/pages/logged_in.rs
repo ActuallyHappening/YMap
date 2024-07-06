d86f0568614b11cb0c65a775faa4c49facf37cad
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-07-06 17:15:06 +1000

    add: session_info resource

--- website/src/pages/logged_in.rs
+++ website/src/pages/logged_in.rs
@@ -1,11 +1,20 @@
-use crate::{app_state, prelude::*};
+use yauth::cmds::session_info::SessionInfo;
+
+use crate::{app_state, prelude::*, AppError};
+
+pub async fn is_logged_in() -> Result<SessionInfo, AppError> {
+	let state = app_state();
+	let config = state.config().await;
+	let db = state.db().await?;
+	let session_info = config.control_db(&db).session_info().await?;
+	Ok(session_info)
+}
 
 #[component]
 pub fn LoggedIn() -> impl IntoView {
 	let state = app_state();
-	create_effect(move |_| {
-		info!("Loaded the Logged in page");
-	});
+	let session_info = create_resource(|| (), |_| async move { is_logged_in.await });
+
 	view! {
 		<h1>"Checking auth status ..."</h1>
 	}
