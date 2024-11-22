8c9ff43b9520267df32a291419ae0230cd5ec4c6
Caleb Yates <105958073+ActuallyHappening@users.noreply.github.com>
2024-11-22 15:24:35 +1000

    refactor: organizing top level lib.rs

---
+++ src/assets.rs
@@ -0,0 +1,4 @@
+pub mod fonts {
+    pub const VICTOR_MONO_REFULAR_TTF: &[u8] =
+        include_bytes!("../assets/fonts/VictorMono-Regular.ttf");
+}
---
+++ src/pastebin.rs
@@ -0,0 +1,70 @@
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+use bevy_cosmic_edit::{
+    cosmic_text::{Attrs, Family, Metrics},
+    *,
+};
+
+fn setup(
+    mut commands: Commands,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut font_system: ResMut<CosmicFontSystem>,
+) {
+    let primary_window = windows.single();
+    let camera_bundle = Camera2dBundle {
+        camera: Camera {
+            clear_color: ClearColorConfig::Custom(Color::WHITE),
+            ..default()
+        },
+        ..default()
+    };
+    commands.spawn(camera_bundle);
+
+    let mut attrs = Attrs::new();
+    attrs = attrs.family(Family::Name("Victor Mono"));
+    attrs = attrs.color(CosmicColor::rgb(0x94, 0x00, 0xD3));
+
+    let cosmic_edit = (CosmicEditBundle {
+        buffer: CosmicBuffer::new(&mut font_system, Metrics::new(14., 18.)).with_text(
+            &mut font_system,
+            "😀😀😀 x => y",
+            attrs,
+        ),
+        sprite_bundle: SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(primary_window.width(), primary_window.height())),
+                ..default()
+            },
+            ..default()
+        },
+        ..default()
+    },);
+
+    let cosmic_edit = commands.spawn(cosmic_edit).id();
+
+    commands.insert_resource(FocusedWidget(Some(cosmic_edit)));
+}
+
+pub fn plugin(app: &mut App) {
+    let font_bytes: &[u8] = crate::assets::fonts::VICTOR_MONO_REFULAR_TTF;
+    let font_config = CosmicFontConfig {
+        fonts_dir_path: None,
+        font_bytes: Some(vec![font_bytes]),
+        load_system_fonts: true,
+    };
+
+    app.add_plugins(CosmicEditPlugin {
+        font_config,
+        ..default()
+    })
+    .add_systems(Startup, setup)
+    .add_systems(
+        Update,
+        (
+            print_editor_text,
+            change_active_editor_sprite,
+            deselect_editor_on_esc,
+        ),
+    );
+}
