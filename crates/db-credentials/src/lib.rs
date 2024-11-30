250137a9fa67d4a57a78237f0e9478bc9b0de3d1
Caleb Yates <calebyates42@gmail.com>
2024-11-30 18:26:31 +1000

    todo: implement background server start

--- crates/db-credentials/src/lib.rs
+++ crates/db-credentials/src/lib.rs
@@ -4,3 +4,6 @@ pub const DB_USERNAME: &str = DB_TOML.username;
 pub const DB_PASSWORD: &str = DB_TOML.password;
 pub const DB_NAMESPACE: &str = DB_TOML.namespace;
 pub const DB_DATABASE: &str = DB_TOML.database;
+pub const START_COMMAND: &str = DB_TOML.start_command;
+pub const CLEAN_COMMAND: &str = DB_TOML.clean_command;
+pub const CONNECT_ENDPOINT: &str = DB_TOML.external_connect_endpoint;
