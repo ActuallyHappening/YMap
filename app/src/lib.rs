fcc30d4b1a881d533180fdd046763bf29dbd3ac8
Caleb Yates <calebyates42@gmail.com>
2025-04-02 21:27:13 +1000

    feat: website loads well!

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -3,9 +3,10 @@ pub mod main {
   use crate::prelude::*;
 
   pub fn main() -> color_eyre::Result<()> {
+    info!("Hydrating ...");
     leptos::mount::mount_to_body(|| view! { <h1> "Yay!" </h1> });
-
     info!("Finished hydration");
+
     Ok(())
   }
 }
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,9 @@
 use app::prelude::*;
 
 fn main() -> color_eyre::Result<()> {
+  console_error_panic_hook::set_once();
   utils::tracing::install_tracing("info,app=trace")?;
+
   info!("Initialized logging on front-end");
 
   app::main::main()
