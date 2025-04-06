13913c5add970d00983c728e3a01750324f53306
Caleb Yates <calebyates42@gmail.com>
2025-04-06 18:00:56 +1000

    todo: make dinner not over engineer macros!

--- thing/macros/src/lib.rs
+++ thing/macros/src/lib.rs
@@ -6,8 +6,10 @@
 //     .into()
 // }
 
+use darling::{FromDeriveInput, FromMeta};
+use proc_macro2::Span;
 use syn::token::{Enum, Token, Union};
-use syn::{Data, DataEnum, DataUnion, Error};
+use syn::{Data, DataEnum, DataUnion, Error, Fields, Ident};
 
 use syn::{DeriveInput, parse_macro_input};
 
@@ -18,7 +20,7 @@ fn payload_impl(input: syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream
 
   Ok(match input.data {
     syn::Data::Struct(data) => {
-      let fields = data.fields;
+      let fields = extract_field_info(data.fields)?;
 
       todo!();
     }
@@ -31,4 +33,61 @@ fn payload_impl(input: syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream
       ..
     }) => return Err(Error::new(span, "Can only be used on structs")),
   })
+}
+
+struct MyField {
+  written_name: Ident,
+  renamed_link: syn::LitStr,
+}
+
+#[derive(FromMeta)]
+struct ValidAttrs {
+  rename: RenameAttr,
+}
+
+/// Attrs to parse
+#[derive(Clone, FromMeta)]
+struct RenameAttr {
+  #[darling(rename = "fn")]
+  renamed_link: syn::LitStr,
+}
+
+fn extract_field_info(fields: Fields) -> Result<Vec<MyField>, Error> {
+  match fields {
+    Fields::Unit => Err(Error::new(Span::call_site(), "Only works on named fields")),
+    Fields::Named(fields) => {
+      let mut ret = Vec::new();
+      for field in fields.named.iter() {
+        let written_name = field.ident.as_ref().unwrap();
+
+        let mut valid_attrs: Option<ValidAttrs> = None;
+        for attr_line in field.attrs.iter() {
+          let meta = &attr_line.meta;
+          let my_attrs = ValidAttrs::from_meta(meta)?;
+          match valid_attrs {
+            Some(_prev) => {
+              // todo: propagate prev span
+              return Err(Error::new(Span::call_site(), "Cannot rename twice"));
+            }
+            None => {
+              valid_attrs = Some(my_attrs);
+            }
+          }
+        }
+
+        ret.push(MyField {
+          written_name: written_name.clone(),
+          renamed_link: valid_attrs
+            .ok_or(Error::new(
+              written_name.span(),
+              "Must provide a rename(fn) attr",
+            ))?
+            .rename
+            .renamed_link,
+        })
+      }
+      Ok(ret)
+    }
+    Fields::Unnamed(fields) => Err(Error::new(Span::call_site(), "Only works on named fields")),
+  }
 }
