cbdf924160fe0fd48883b84d2ad3c12ccc0c38d3
Caleb Yates <calebyates42@gmail.com>
2025-04-09 22:36:20 +1000

    todo: think and sleep

--- app/src/db.rs
+++ app/src/db.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use db::auth;
 use generic_err::GenericErrorExt;
 
@@ -25,4 +27,34 @@ impl DbConn {
       DbConn::Err(err) => Err(Error::DbError(GenericError::from_ref(err))).make_generic(),
     }
   }
+}
+
+pub fn Connect() -> impl IntoView {
+  let state = DbConn::from_context();
+  let newconn = LocalResource::new(move || {
+    let state = state.read();
+    let conn = matches!(state.deref(), DbConn::WaitingForGuest { prev_err: None });
+    async {
+      if conn {
+        Some((async move || -> Result<Db<auth::NoAuth>, db::Error> {
+          let db = db::Db::build().wss()?.await;
+
+          Ok(todo!())
+        })())
+      } else {
+        None
+      }
+    }
+  });
+  let ui = move || {
+    let state = state.read();
+    let state = state.deref();
+    match state {
+      DbConn::WaitingForGuest { prev_err: None } => {
+        // start connection
+      }
+      _ => todo!(),
+    }
+  };
+  ui
 }
