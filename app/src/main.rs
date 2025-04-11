d8d3634ae3e1627e17c84dcaa4cfedc6320d77d6
Caleb Yates <calebyates42@gmail.com>
2025-04-11 16:50:56 +1000

    repro: almost at minimum size

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,9 +2,7 @@
 
 pub fn main() {
   console_error_panic_hook::set_once();
-  utils::tracing::install_tracing(
-    "debug,app=trace,mathquill-leptos=trace,mathquill-js=trace,mathquill-js-sys=trace,latex-parser=trace,cas=trace",
-  ).unwrap();
+  utils::tracing::install_tracing("debug").unwrap();
 
   tracing::info!("Initialized logging on front-end");
 
@@ -19,8 +17,6 @@ pub use utils::prelude::*;
 pub type AppResult<T> = Result<T, AppError>;
 
 pub fn App() -> impl IntoView {
-  provide_context(RootOwner(Owner::current().unwrap()));
-
   provide_context(RwSignal::new(AppError::StartsOffHere));
 
   Effect::new(move || {
@@ -36,15 +32,10 @@ pub fn App() -> impl IntoView {
   }
 }
 
-#[derive(Clone, Debug)]
-pub struct LatexDemoPage {
-  pub doesnt_exist: String,
-}
-
 #[component]
 pub fn LatexDemo() -> impl IntoView {
   move || {
-    let err = get_err_from_context();
+    let err = expect_context::<RwSignal<AppError>>().get();
     debug!(?err, "Rendering error in ui");
     Result::<(), _>::Err(err)
   }
@@ -61,30 +52,6 @@ pub fn MyErrorBoundary(children: Children) -> impl IntoView {
       .collect_view()
   };
   view! { <leptos::error::ErrorBoundary fallback>{children()}</leptos::error::ErrorBoundary> }
-}
-
-#[derive(Clone)]
-struct RootOwner(Owner);
-
-pub fn get_err_from_context() -> AppError {
-  if let Some(s) = use_context::<RwSignal<AppError>>() {
-    let ret = s.get();
-    debug!(?ret, "Retrieved cached value");
-    return ret;
-  }
-  debug!("Didn't hit cache, loading for the first time");
-
-  let root_owner: Owner = use_context::<RootOwner>().unwrap().0;
-  root_owner.with(|| {
-    Effect::new(move || {
-      use_context::<RwSignal<AppError>>()
-        .unwrap()
-        .set(AppError::RenderMePlease);
-    });
-  });
-
-  // subscribes
-  use_context::<RwSignal<AppError>>().unwrap().get()
 }
 
 #[derive(Debug, thiserror::Error, Clone)]
