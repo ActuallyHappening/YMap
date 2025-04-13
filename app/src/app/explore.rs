4f92d07fc4a644e50a280a4f8823b66e85c091b7
Caleb Yates <calebyates42@gmail.com>
2025-04-13 16:19:18 +1000

    feat: root nodes load!

--- app/src/app/explore.rs
+++ app/src/app/explore.rs
@@ -16,9 +16,18 @@ pub fn Explore() -> impl IntoView {
         .collect_view()
     })
   };
+  let cls = style! {
+    div {
+      flex-direction: row;
+      flex-wrap: wrap;
+      align-items: stretch;
+    }
+  };
   view! {
     <h1> "Explore the YMap knowledge database"</h1>
-    { thing_previews.handle_error() }
+    <div class=cls>
+      { thing_previews.handle_error() }
+    </div>
   }
 }
 
@@ -27,11 +36,13 @@ fn ThingPreview(#[prop(into)] id: Signal<ThingId>) -> impl IntoView {
   let description = load_payload::<DocumentedPayload>(id);
   let cls = style! {
     div {
-      max-width: 6rem;
+      display: block;
+      width: 15rem;
+      height: 10rem;
       border: 1px solid black;
     }
-    div > p {
-      text-wrap: nowrap;
+    div > :deep(p), :deep(pre) {
+      // text-wrap: nowrap;
       text-overflow: ellipsis;
       overflow: hidden;
     }
@@ -39,14 +50,15 @@ fn ThingPreview(#[prop(into)] id: Signal<ThingId>) -> impl IntoView {
   let ui = move || -> AppResult<_> {
     let desc = description.get()?;
     let title = desc.payload().name.to_string();
-    let description = desc.payload().description.to_string();
+    // let description = desc.payload().description.to_string();
     Ok(view! {
       <h2>{title}</h2>
-      <p>{description}</p>
+      // <p>{description}</p>
     })
   };
   view! {
-    <div class=cls>
+    class=cls,
+    <div>
       { ui.handle_error() }
     </div>
   }
--- app/styles/build.nu
+++ app/styles/build.nu
@@ -1,1 +1,5 @@
+# In nushell, to watch
+# trunk serve
+# watch . --glob=**/*.rs {|| nu styles/build.nu}
+
 RUST_LOG=info stylers --output-path /home/ah/Desktop/YMap/app/styles/stylers.css --search-dir /home/ah/Desktop/YMap/app/src
