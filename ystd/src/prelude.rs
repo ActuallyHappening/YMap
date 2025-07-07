c4f3d218975701c0103e72970b5d3f620b425be7
Caleb Yates <calebyates42@gmail.com>
2025-07-07 13:41:21 +1000

    refactor: move prelude

---
+++ ystd/src/prelude.rs
@@ -0,0 +1,11 @@
+pub use crate::assert::{
+	eyre_assert, eyre_assert as assert, eyre_assert_eq, eyre_assert_eq as assert_eq,
+};
+pub(crate) use crate::io::MapIoError as _;
+pub use crate::path::{Path, PathBuf, Utf8Path, Utf8PathBuf, YPath, YPathBuf};
+
+pub(crate) use color_eyre::Report;
+pub(crate) use color_eyre::eyre::{WrapErr as _, bail, eyre};
+pub(crate) use std::sync::Arc;
+pub use tracing::{debug, error, info, trace, warn};
+pub use crate::{fs, env, path, io};
