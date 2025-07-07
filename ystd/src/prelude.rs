1ff2a64152c55d2885356aa43d5194db228b55b5
Caleb Yates <calebyates42@gmail.com>
2025-07-07 07:27:14 +0000

    fix: prelude better

--- ystd/src/prelude.rs
+++ ystd/src/prelude.rs
@@ -1,14 +1,17 @@
-pub(crate) use crate::io::MapIoError as _;
 pub(crate) use crate::error::WrapReportedErr as _;
+pub(crate) use crate::io::MapIoError as _;
 
 pub(crate) use color_eyre::Report;
-pub(crate) use color_eyre::eyre::{WrapErr as _, bail, eyre};
 pub(crate) use std::sync::Arc;
 
+// public internal deps
 pub use crate::assert::{
 	eyre_assert, eyre_assert as assert, eyre_assert_eq, eyre_assert_eq as assert_eq,
 };
 pub use crate::path::{Utf8Path, Utf8PathBuf};
 pub use crate::{env, fs, io, path};
+
+// public external deps
+pub use color_eyre::eyre::{WrapErr as _, bail, eyre};
 pub use extension_traits::extension;
 pub use tracing::{debug, error, info, trace, warn};
