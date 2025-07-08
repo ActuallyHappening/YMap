91053159ba9e41ebbbdd33e1148d7c85eeb168b6
Caleb Yates <calebyates42@gmail.com>
2025-07-08 10:55:30 +0000

    add: hex::decode err

--- ystd/src/hex.rs
+++ ystd/src/hex.rs
@@ -1,1 +1,13 @@
 pub use hex::*;
+
+use crate::error::{ReportedError, WrapReportedErr};
+use crate::prelude::*;
+
+#[instrument(name = "ystd::hex::decode")]
+pub fn decode<T: AsRef<[u8]> + core::fmt::Debug>(
+	input: T,
+) -> Result<Vec<u8>, ReportedError<FromHexError>> {
+	::hex::decode(input)
+		.map_err(ReportedError::new)
+		.wrap_reported_err("Couldn't decode hex encoded data")
+}
