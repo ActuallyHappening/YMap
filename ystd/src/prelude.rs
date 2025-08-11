c1e7d43d298bd50236c574ed035d3fca6aafdc0a
Caleb Yates <calebyates42@gmail.com>
2025-08-11 05:14:06 +0000

    add: exports color_eyre

--- ystd/src/prelude.rs
+++ ystd/src/prelude.rs
@@ -5,9 +5,7 @@ pub(crate) use color_eyre::Report;
 pub(crate) use std::sync::Arc;
 
 // public internal deps
-pub use crate::assert::{
-	eyre_assert, eyre_assert as assert, eyre_assert_eq, eyre_assert_eq as assert_eq,
-};
+pub use crate::assert::{eyre_assert, eyre_assert, eyre_assert_eq, eyre_assert_eq};
 pub use crate::path::{Utf8Path, Utf8PathBuf};
 pub use crate::{env, fs, io, path};
 
@@ -15,6 +13,10 @@ pub use crate::{env, fs, io, path};
 pub use color_eyre::eyre::{WrapErr as _, bail, eyre};
 pub use extension_traits::extension;
 pub use tracing::{debug, error, info, instrument, span, trace, warn};
+
+// public crate re-exports
+pub use color_eyre;
+pub use tracing;
 
 // publilc extension traits,
 // this may clobber the global namespace to much
