d1e6611c0a921742ac376d1a64bbdc7e12d13602
Caleb Yates <calebyates42@gmail.com>
2025-08-11 05:14:37 +0000

    fix: eyre assert reexports

--- ystd/src/prelude.rs
+++ ystd/src/prelude.rs
@@ -5,7 +5,7 @@ pub(crate) use color_eyre::Report;
 pub(crate) use std::sync::Arc;
 
 // public internal deps
-pub use crate::assert::{eyre_assert, eyre_assert, eyre_assert_eq, eyre_assert_eq};
+pub use crate::assert::{eyre_assert, eyre_assert_eq};
 pub use crate::path::{Utf8Path, Utf8PathBuf};
 pub use crate::{env, fs, io, path};
 
