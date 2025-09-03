f6fc97231aec3cc2132c6f8be5c9681631f06088
Caleb Yates <calebyates42@gmail.com>
2025-09-03 15:16:17 +1000

    add

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,12 @@ fn main() {
 	debug!("Logging started");
 
 	info!("Hello, world!");
+
+	let descriptor = wgpu::InstanceDescriptor::default();
+	let instance = wgpu::Instance::new(&descriptor);
+
+	let target = todo!();
+	let surface = instance
+		.create_surface(target)
+		.wrap_err("Couldn't create surface")?;
 }
