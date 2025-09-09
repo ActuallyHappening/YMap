107949506f52fd1cec6af435853ceef19cb87e88
Caleb Yates <calebyates42@gmail.com>
2025-09-09 12:28:28 +0000

    todo: understand wgpu API to abuse it

--- src/lib.rs
+++ src/lib.rs
@@ -192,7 +192,7 @@ impl SetupApp {
 				depth_slice: None,
 				resolve_target: None,
 				ops: wgpu::Operations {
-					load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+					load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
 					store: wgpu::StoreOp::Store,
 				},
 			})],
