c05727027d18af5ad7acfaa66b89573bfbe13575
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2025-11-08 17:04:01 +1000

    Initial Commit

---
+++ YMap/YMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YMapApp.swift
+//  YMap
+//
+//  Created by Caleb Yates on 8/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct YMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
